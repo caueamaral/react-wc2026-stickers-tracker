e03a130ae6e9bcb628d8dba9d5d9a24ed9674472
Cauê Amaral <caue.amaral@hotmail.com>
2026-05-27 15:29:53 -0300

    chore: create Header, Main, and Groups components

---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+export function Header() {
+    return (
+        <header>
+            <h1>React WC2026 Stickers Tracker</h1>
+        </header>
+    )
+}
---
+++ src/components/Main.tsx
@@ -0,0 +1,9 @@
+import { Groups } from './Groups'
+
+export function Main() {
+    return (
+        <main>
+            <Groups />
+        </main>
+    )
+}
