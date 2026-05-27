cedfa4f2bf217c799fe706587b53cf62ac4c3709
Cauê Amaral <caue.amaral@hotmail.com>
2026-05-27 15:38:00 -0300

    chore: install tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Header } from './components/Header'
 import { Main } from './components/Main'
+import './css/styles.css'
 
 function App() {
   return (
--- src/components/Groups.tsx
+++ src/components/Groups.tsx
@@ -1,7 +1,25 @@
 export function Groups() {
     return (
         <article>
-            Groups
+            <section>
+                <header>
+                    <h2 className="text-2xl">Group A</h2>
+                </header>
+                <div>
+                    <div>
+                        México
+                    </div>
+                    <div>
+                        África do Sul
+                    </div>
+                    <div>
+                        Coréia do Sul
+                    </div>
+                    <div>
+                        República Tcheca
+                    </div>
+                </div>
+            </section>
         </article>
     )
 }
