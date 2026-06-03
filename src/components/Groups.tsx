15295e62e09b2d8adb493f079f028b0f03b91f4e
Cauê Amaral <caue.amaral@hotmail.com>
2026-06-03 16:06:28 -0300

    refactor: move sticker and group data into separate files

--- src/components/Groups.tsx
+++ src/components/Groups.tsx
@@ -1,57 +1,7 @@
+import { stickers } from '../data/stickers'
+import { groups } from '../data/groups'
+
 export function Groups() {
-    const stickers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-
-    const groups = [
-        {
-            name: 'A',
-            teams: ['México', 'África do Sul', 'Coreia do Sul', 'República Checa']
-        },
-        {
-            name: 'B',
-            teams: ['Canadá', 'Bósnia e Herzegovina', 'Catar', 'Suíça']
-        },
-        {
-            name: 'C',
-            teams: ['Brasil', 'Marrocos', 'Haiti', 'Escócia']
-        },
-        {
-            name: 'D',
-            teams: ['Estados Unidos', 'Paraguai', 'Austrália', 'Turquia']
-        },
-        {
-            name: 'E',
-            teams: ['Alemanha', 'Curaçao', 'Costa do Marfim', 'Equador']
-        },
-        {
-            name: 'F',
-            teams: ['Países Baixos', 'Japão', 'Suécia', 'Tunísia']
-        },
-        {
-            name: 'G',
-            teams: ['Bélgica', 'Egito', 'Irã', 'Nova Zelândia']
-        },
-        {
-            name: 'H',
-            teams: ['Espanha', 'Cabo Verde', 'Arábia Saudita', 'Uruguai']
-        },
-        {
-            name: 'I',
-            teams: ['França', 'Senegal', 'Iraque', 'Noruega']
-        },
-        {
-            name: 'J',
-            teams: ['Argentina', 'Argélia', 'Áustria', 'Jordânia']
-        },
-        {
-            name: 'K',
-            teams: ['Portugal', 'RD Congo', 'Uzbequistão', 'Colômbia']
-        },
-        {
-            name: 'L',
-            teams: ['Inglaterra', 'Croácia', 'Gana', 'Panamá']
-        }
-    ]
-
     return (
         <article className="flex flex-wrap justify-center gap-10 mx-auto">
             {
---
+++ src/data/groups.ts
@@ -0,0 +1,55 @@
+export type Group = {
+    name: string,
+    teams: string[]
+}
+
+export const groups: Group[] = [
+    {
+        name: 'A',
+        teams: ['México', 'África do Sul', 'Coreia do Sul', 'República Checa']
+    },
+    {
+        name: 'B',
+        teams: ['Canadá', 'Bósnia e Herzegovina', 'Catar', 'Suíça']
+    },
+    {
+        name: 'C',
+        teams: ['Brasil', 'Marrocos', 'Haiti', 'Escócia']
+    },
+    {
+        name: 'D',
+        teams: ['Estados Unidos', 'Paraguai', 'Austrália', 'Turquia']
+    },
+    {
+        name: 'E',
+        teams: ['Alemanha', 'Curaçao', 'Costa do Marfim', 'Equador']
+    },
+    {
+        name: 'F',
+        teams: ['Países Baixos', 'Japão', 'Suécia', 'Tunísia']
+    },
+    {
+        name: 'G',
+        teams: ['Bélgica', 'Egito', 'Irã', 'Nova Zelândia']
+    },
+    {
+        name: 'H',
+        teams: ['Espanha', 'Cabo Verde', 'Arábia Saudita', 'Uruguai']
+    },
+    {
+        name: 'I',
+        teams: ['França', 'Senegal', 'Iraque', 'Noruega']
+    },
+    {
+        name: 'J',
+        teams: ['Argentina', 'Argélia', 'Áustria', 'Jordânia']
+    },
+    {
+        name: 'K',
+        teams: ['Portugal', 'RD Congo', 'Uzbequistão', 'Colômbia']
+    },
+    {
+        name: 'L',
+        teams: ['Inglaterra', 'Croácia', 'Gana', 'Panamá']
+    }
+]
