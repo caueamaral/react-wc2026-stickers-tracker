9fc4ceb345890cc57d33a092ab6f70153660b6cd
Cauê Amaral <caue.amaral@hotmail.com>
2026-06-08 10:16:35 -0300

    refactor: add HTML structure tags

--- src/components/Groups.tsx
+++ src/components/Groups.tsx
@@ -3,62 +3,53 @@ import { groups } from '../data/groups'
 
 export function Groups() {
     return (
-        <article className="flex flex-col items-center gap-10">
-            {
-                groups.map(group => (
-                    <section
-                        key={group.letter}
-                        className="bg-white p-5 rounded-md w-100"
-                    >
-                        <div className="flex flex-col gap-3">
-                            <header>
-                                <h2 className="bg-neutral-600 text-white text-2xl font-medium text-center uppercase p-5">
-                                    Group {group.letter}
-                                </h2>
-                            </header>
-                            <div className="flex flex-col gap-10">
-                                {group.teams.map(team => (
-                                    <div key={team.name}>
-                                        <div className="font-bold py-3">
-                                            {team.code} - {team.name} {team.flag}
-                                        </div>
-                                        <div className="flex flex-col gap-5">
-                                            <div className="flex gap-5">
-                                                {stickers.slice(0, 5).map(number => (
-                                                    <span key={number} className="bg-neutral-100 px-2 py-4 text-center flex-1 w-[10%] cursor-pointer rounded-full number selected">
-                                                        {number}
-                                                    </span>
-                                                ))}
-                                            </div>
-                                            <div className="flex gap-5">
-                                                {stickers.slice(5, 10).map(number => (
-                                                    <span key={number} className="bg-gray-100 px-2 py-4  text-center flex-1 w-[10%] cursor-pointer rounded-full number">
-                                                        {number}
-                                                    </span>
-                                                ))}
-                                            </div>
-                                            <div className="flex gap-5">
-                                                {stickers.slice(10, 15).map(number => (
-                                                    <span key={number} className="bg-gray-100 px-2 py-4 text-center flex-1 w-[10%] cursor-pointer rounded-full number">
-                                                        {number}
-                                                    </span>
-                                                ))}
-                                            </div>
-                                            <div className="flex gap-5">
-                                                {stickers.slice(15, 20).map(number => (
-                                                    <span key={number} className="bg-gray-100 px-2 py-4 text-center flex-1 w-[10%] cursor-pointer rounded-full number">
-                                                        {number}
-                                                    </span>
-                                                ))}
-                                            </div>
-                                        </div>
-                                    </div>
-                                ))}
+        groups.map(group => (
+            <article key={group.letter} className="bg-white gap-10 p-5 rounded-md w-100">
+                <header>
+                    <h2 className="bg-neutral-600 text-white text-2xl font-medium text-center uppercase p-5">
+                        Group {group.letter}
+                    </h2>
+                </header>
+                <main className="flex flex-col gap-10">
+                    {group.teams.map(team => (
+                        <section key={team.name} className="flex flex-col">
+                            <div className="font-bold py-3">
+                                {team.code} - {team.name} {team.flag}
                             </div>
-                        </div>
-                    </section>
-                ))
-            }
-        </article>
+                            <div className="flex flex-col gap-5">
+                                <div className="flex gap-5">
+                                    {stickers.slice(0, 5).map(number => (
+                                        <span key={number} className="bg-neutral-100 px-2 py-4 text-center flex-1 w-[10%] cursor-pointer rounded-full number">
+                                            {number}
+                                        </span>
+                                    ))}
+                                </div>
+                                <div className="flex gap-5">
+                                    {stickers.slice(5, 10).map(number => (
+                                        <span key={number} className="bg-gray-100 px-2 py-4  text-center flex-1 w-[10%] cursor-pointer rounded-full number">
+                                            {number}
+                                        </span>
+                                    ))}
+                                </div>
+                                <div className="flex gap-5">
+                                    {stickers.slice(10, 15).map(number => (
+                                        <span key={number} className="bg-gray-100 px-2 py-4 text-center flex-1 w-[10%] cursor-pointer rounded-full number">
+                                            {number}
+                                        </span>
+                                    ))}
+                                </div>
+                                <div className="flex gap-5">
+                                    {stickers.slice(15, 20).map(number => (
+                                        <span key={number} className="bg-gray-100 px-2 py-4 text-center flex-1 w-[10%] cursor-pointer rounded-full number">
+                                            {number}
+                                        </span>
+                                    ))}
+                                </div>
+                            </div>
+                        </section>
+                    ))}
+                </main>
+            </article>
+        ))
     )
 }
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,7 +2,7 @@ import { Groups } from './Groups'
 
 export function Main() {
     return (
-        <main>
+        <main className="flex flex-col items-center gap-10">
             <Groups />
         </main>
     )
