aef89a615799d039f2e9299a7d759fae8c4e654f
Cauê Amaral <caue.amaral@hotmail.com>
2026-06-08 10:20:09 -0300

    refactor: improve header semantics with abbr tag

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ export function Header() {
     return (
         <header>
             <h1 className="text-white font-medium text-center text-3xl mb-8">
-                FIFA World Cup 2026
+                <abbr title="Fédération Internationale de Football Association">FIFA</abbr> World Cup 2026
                 <br />
                 Stickers Tracker
             </h1>
