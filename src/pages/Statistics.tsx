5fc0bf19d70d912761516d0921e3709aef156a21
Cauê Amaral <caue.amaral@hotmail.com>
2026-06-18 15:35:22 -0300

    feat: calculate stickers statistics

--- src/pages/Statistics.tsx
+++ src/pages/Statistics.tsx
@@ -1,18 +1,61 @@
+import { groups } from '../data/groups'
+import { cocaColaStickers, fwcStickers, groupStickers } from '../data/stickers'
+
+type SelectedStickers = Record<string, number[]>
+
 export function Statistics() {
-    const totalStickers = 980
-    const completedStickers = 0
-    const incompleteStickers = 0
+    const teams = groups.reduce(
+        (total, group) => total + group.teams.length,
+        0
+    )
+
+    const totalStickers =
+        teams * groupStickers.length +
+        fwcStickers.length +
+        cocaColaStickers.length
+    
+    const stored = localStorage.getItem('selectedStickers')
+    let selectedStickers: SelectedStickers = {}
+
+    if (stored) {
+        try {
+            selectedStickers = JSON.parse(stored)
+        }
+        catch {
+            selectedStickers = {}
+        }
+    }
+
+    const completedStickers = Object.values(selectedStickers).reduce(
+        (total, stickers) => total + stickers.length,
+        0
+    )
+
+    const missingStickers = totalStickers - completedStickers
+
+    const percentageCompleted = (completedStickers / totalStickers * 100).toFixed(2).replace('.', ',')
 
     return (
-        <main className="flex flex-col items-center justify-center gap-10 px-10">
-            <article className="bg-white p-4 rounded-md w-90">
-                Total: {totalStickers}
-            </article>
-            <article className="bg-white p-4 rounded-md w-90">
-                Completed: {completedStickers}
-            </article>
-            <article className="bg-white p-4 rounded-md w-90">
-                Incomplete: {incompleteStickers}
+        <main className="flex flex-col items-center justify-center gap-5 px-10">
+            <article className="flex flex-col gap-5 w-90">
+                <section className="bg-white p-4 text-lg rounded-md">
+                    <div>
+                        {totalStickers} stickers in total
+                    </div>
+                    <div>
+                        <span className="text-green-600">
+                            {completedStickers} stickers completed
+                        </span>
+                    </div>
+                    <div>
+                        <span className="text-red-600">
+                            {missingStickers} missing stickers
+                        </span>
+                    </div>
+                </section>
+                <section className="bg-white p-4 text-2xl text-center font-medium rounded-md">
+                    {percentageCompleted}% completed
+                </section>
             </article>
         </main>
     )
